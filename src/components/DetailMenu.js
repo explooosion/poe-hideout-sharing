7a0b3e2087748ee005823baf45b5e3ae64137a18
Robby <ta7382@gmail.com>
2019-01-03 12:47:39 +0800

    Fix css scope

--- src/components/DetailMenu.js
+++ src/components/DetailMenu.js
@@ -22,32 +22,34 @@ class DetailMenu extends Component {
     const { author, type, favour, version, update, create } = this.state.hideout;
     return (
       <MenuLayout title={this.state.title}>
-        <div className="list">
-          <div className="item">
-            <h4 className="item-title">Hideout Type</h4>
-            <h4 className="item-value">{type} Hideout</h4>
-          </div>
-          <div className="item">
-            <h4 className="item-title">Total Favor</h4>
-            <h4 className="item-value">
-              <NumberFormat value={favour} displayType={'text'} thousandSeparator={true} />
-            </h4>
-          </div>
-          <div className="item">
-            <h4 className="item-title">Author</h4>
-            <h4 className="item-value">{author}</h4>
-          </div>
-          <div className="item">
-            <h4 className="item-title">Version</h4>
-            <h4 className="item-value">{version}</h4>
-          </div>
-          <div className="item">
-            <h4 className="item-title">Update</h4>
-            <h4 className="item-value">{update}</h4>
-          </div>
-          <div className="item">
-            <h4 className="item-title">Create</h4>
-            <h4 className="item-value">{create}</h4>
+        <div className="detail-menu">
+          <div className="list">
+            <div className="item">
+              <h4 className="item-title">Hideout Type</h4>
+              <h4 className="item-value">{type} Hideout</h4>
+            </div>
+            <div className="item">
+              <h4 className="item-title">Total Favor</h4>
+              <h4 className="item-value">
+                <NumberFormat value={favour} displayType={'text'} thousandSeparator={true} />
+              </h4>
+            </div>
+            <div className="item">
+              <h4 className="item-title">Author</h4>
+              <h4 className="item-value">{author}</h4>
+            </div>
+            <div className="item">
+              <h4 className="item-title">Version</h4>
+              <h4 className="item-value">{version}</h4>
+            </div>
+            <div className="item">
+              <h4 className="item-title">Update</h4>
+              <h4 className="item-value">{update}</h4>
+            </div>
+            <div className="item">
+              <h4 className="item-title">Create</h4>
+              <h4 className="item-value">{create}</h4>
+            </div>
           </div>
         </div>
       </MenuLayout>
--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -159,118 +159,120 @@ class HomeMenu extends Component {
   render() {
     return (
       <MenuLayout title={this.state.title}>
-        <div className="item">
-          <h4 className="item-title">Hideout Type</h4>
-          <Dropdown
-            style={{ width: '100%' }}
-            value={this.state.type}
-            options={this.state.types}
-            onChange={(e) => this.onFilterChange({ type: e.value })}
-            filter={true}
-            filterPlaceholder="Filter..."
-            placeholder="Select type"
-            itemTemplate={this.renderHideoutTemplate}
-            showClear={true}
-          // selectedItemTemplate={this.renderHideoutSelectedTemplate}
-          />
+        <div className="home-menu">
+          <div className="item">
+            <h4 className="item-title">Hideout Type</h4>
+            <Dropdown
+              style={{ width: '100%' }}
+              value={this.state.type}
+              options={this.state.types}
+              onChange={(e) => this.onFilterChange({ type: e.value })}
+              filter={true}
+              filterPlaceholder="Filter..."
+              placeholder="Select type"
+              itemTemplate={this.renderHideoutTemplate}
+              showClear={true}
+            // selectedItemTemplate={this.renderHideoutSelectedTemplate}
+            />
+          </div>
+          <Fieldset legend="Options" toggleable={true} collapsed={this.state.panelOptions} onToggle={(e) => this.setState({ panelOptions: e.value })}>
+            <div className="item">
+              <h4 className="item-title">MTX</h4>
+              <Dropdown
+                style={{ width: '100%' }}
+                value={this.state.mtx}
+                options={this.state.mtxs}
+                onChange={(e) => this.onSelectChange({ mtx: e.value })}
+                placeholder="Select MTX"
+              />
+            </div>
+            <div className="item">
+              <h4 className="item-title">Minimum Rating</h4>
+              <Dropdown
+                style={{ width: '100%' }}
+                value={this.state.mini_rating}
+                options={this.state.mini_ratings}
+                onChange={(e) => this.onSelectChange({ mini_rating: e.value })}
+                placeholder="Select Minimum Rating"
+              />
+            </div>
+            <div className="item">
+              <h4 className="item-title">Alva Level</h4>
+              <Dropdown
+                style={{ width: '100%' }}
+                value={this.state.alva_level}
+                options={this.state.alva_levels}
+                onChange={(e) => this.onSelectChange({ alva_level: e.value })}
+                placeholder="Select Alva Level"
+              />
+            </div>
+            <div className="item">
+              <h4 className="item-title">Einhar Level</h4>
+              <Dropdown
+                style={{ width: '100%' }}
+                value={this.state.einhar_level}
+                options={this.state.einhar_levels}
+                onChange={(e) => this.onSelectChange({ einhar_level: e.value })}
+                placeholder="Select Einhar Level"
+              />
+            </div>
+            <div className="item">
+              <h4 className="item-title">Niko Level</h4>
+              <Dropdown
+                style={{ width: '100%' }}
+                value={this.state.niko_level}
+                options={this.state.niko_levels}
+                onChange={(e) => this.onSelectChange({ niko_level: e.value })}
+                placeholder="Select Niko Level"
+              />
+            </div>
+            <div className="item">
+              <h4 className="item-title">Zana Level</h4>
+              <Dropdown
+                style={{ width: '100%' }}
+                value={this.state.zana_level}
+                options={this.state.zana_levels}
+                onChange={(e) => this.onSelectChange({ zana_level: e.value })}
+                placeholder="Select Zana Level"
+              />
+            </div>
+          </Fieldset>
+          <div className="item">
+            <h4 className="item-title">Download</h4>
+            <SelectButton
+              value={this.state.download}
+              options={this.renderSelectButton()}
+              onChange={(e) => this.onSortChange({ download: e.value })}
+            >
+            </SelectButton>
+          </div>
+          <div className="item">
+            <h4 className="item-title">Views</h4>
+            <SelectButton
+              value={this.state.views}
+              options={this.renderSelectButton()}
+              onChange={(e) => this.onSortChange({ views: e.value })}
+            >
+            </SelectButton>
+          </div>
+          <div className="item">
+            <h4 className="item-title">Favorite</h4>
+            <SelectButton
+              value={this.state.favorite}
+              options={this.renderSelectButton()}
+              onChange={(e) => this.onSortChange({ favorite: e.value })}
+            >
+            </SelectButton>
+          </div>
+          {
+            /*
+            <div className="item">
+              <h4 className="item-title">Analysis</h4>
+              <Chart type="doughnut" data={this.getChartData()} />
+            </div>
+            */
+          }
         </div>
-        <Fieldset legend="Options" toggleable={true} collapsed={this.state.panelOptions} onToggle={(e) => this.setState({ panelOptions: e.value })}>
-          <div className="item">
-            <h4 className="item-title">MTX</h4>
-            <Dropdown
-              style={{ width: '100%' }}
-              value={this.state.mtx}
-              options={this.state.mtxs}
-              onChange={(e) => this.onSelectChange({ mtx: e.value })}
-              placeholder="Select MTX"
-            />
-          </div>
-          <div className="item">
-            <h4 className="item-title">Minimum Rating</h4>
-            <Dropdown
-              style={{ width: '100%' }}
-              value={this.state.mini_rating}
-              options={this.state.mini_ratings}
-              onChange={(e) => this.onSelectChange({ mini_rating: e.value })}
-              placeholder="Select Minimum Rating"
-            />
-          </div>
-          <div className="item">
-            <h4 className="item-title">Alva Level</h4>
-            <Dropdown
-              style={{ width: '100%' }}
-              value={this.state.alva_level}
-              options={this.state.alva_levels}
-              onChange={(e) => this.onSelectChange({ alva_level: e.value })}
-              placeholder="Select Alva Level"
-            />
-          </div>
-          <div className="item">
-            <h4 className="item-title">Einhar Level</h4>
-            <Dropdown
-              style={{ width: '100%' }}
-              value={this.state.einhar_level}
-              options={this.state.einhar_levels}
-              onChange={(e) => this.onSelectChange({ einhar_level: e.value })}
-              placeholder="Select Einhar Level"
-            />
-          </div>
-          <div className="item">
-            <h4 className="item-title">Niko Level</h4>
-            <Dropdown
-              style={{ width: '100%' }}
-              value={this.state.niko_level}
-              options={this.state.niko_levels}
-              onChange={(e) => this.onSelectChange({ niko_level: e.value })}
-              placeholder="Select Niko Level"
-            />
-          </div>
-          <div className="item">
-            <h4 className="item-title">Zana Level</h4>
-            <Dropdown
-              style={{ width: '100%' }}
-              value={this.state.zana_level}
-              options={this.state.zana_levels}
-              onChange={(e) => this.onSelectChange({ zana_level: e.value })}
-              placeholder="Select Zana Level"
-            />
-          </div>
-        </Fieldset>
-        <div className="item">
-          <h4 className="item-title">Download</h4>
-          <SelectButton
-            value={this.state.download}
-            options={this.renderSelectButton()}
-            onChange={(e) => this.onSortChange({ download: e.value })}
-          >
-          </SelectButton>
-        </div>
-        <div className="item">
-          <h4 className="item-title">Views</h4>
-          <SelectButton
-            value={this.state.views}
-            options={this.renderSelectButton()}
-            onChange={(e) => this.onSortChange({ views: e.value })}
-          >
-          </SelectButton>
-        </div>
-        <div className="item">
-          <h4 className="item-title">Favorite</h4>
-          <SelectButton
-            value={this.state.favorite}
-            options={this.renderSelectButton()}
-            onChange={(e) => this.onSortChange({ favorite: e.value })}
-          >
-          </SelectButton>
-        </div>
-        {
-          /*
-          <div className="item">
-            <h4 className="item-title">Analysis</h4>
-            <Chart type="doughnut" data={this.getChartData()} />
-          </div>
-          */
-        }
       </MenuLayout>
     );
   }
