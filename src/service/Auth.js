91e920253b933e1e4d86c62884c31386df971cf1
Robby <ta7382@gmail.com>
2019-02-02 14:10:23 +0800

    Fix auth alert for OAuth 2.0

--- src/service/Auth.js
+++ src/service/Auth.js
@@ -4,18 +4,40 @@ import User from '../interface/User';
 
 class Auth {
 
-  user = {};
+  user = null;
+
+  token = '';
 
   userSession = Session.get('auth');
 
   constructor() {
     this.auth = auth;
-    this.provider = new firebase.auth.GoogleAuthProvider();
-    this.provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    this.auth.useDeviceLanguage();
-    this.onAuthStateChanged();
-
     if (this.userSession) this.user = Session.get('auth');
+    // Initial
+    this.provider = new firebase.auth.GoogleAuthProvider();
+    // Do not use OAuth 2.0 from official
+    // this.provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    this.auth.useDeviceLanguage();
+    // Handler
+    this.getRedirectResult();
+    this.onAuthStateChanged();
+  }
+
+  setAuthData(payload = null) {
+    // Payload empty or has user then return
+    if (!payload && this.user) return;
+    const { uid, displayName, photoURL, email } = payload;
+    this.user = new User(uid, displayName, photoURL, email);
+    Session.set('auth-google', this.user);
+  }
+
+  /**
+   * Login with google+
+   */
+  async onSignInByGoogle() {
+    // return this.auth.signInWithPopup(this.provider)
+    return this.auth.signInWithRedirect(this.provider)
+      .catch(error => console.error('onSignInByGoogle', error));
   }
 
   /**
@@ -26,20 +48,22 @@ class Auth {
       if (user === null || !user.providerData) return;
       if (user.providerData.length === 0) return;
       if (user.providerData[0].uid.length === 0) return;
-      const { uid, displayName, photoURL, email } = user.providerData[0];
-      this.user = new User(uid, displayName, photoURL, email);
-      // console.log('onAuthStateChanged', this.user);
-      Session.set('auth-google', this.user);
+      this.setAuthData(user.providerData[0]);
     });
   }
 
   /**
-   * Login with google+
+   * Login after redirect
    */
-  async onSignInByGoogle() {
-    // return this.auth.signInWithPopup(this.provider)
-    return this.auth.signInWithRedirect(this.provider)
-      .catch(error => console.error('onSignInByGoogle', error));
+  getRedirectResult() {
+    this.auth.getRedirectResult().then(result => {
+      if (result.credential && result.user) {
+        if (result.user.providerData.length === 0) return;
+        if (result.user.providerData[0].uid.length === 0) return;
+        this.token = result.credential.accessToken;
+        this.setAuthData(result.user.providerData[0]);
+      }
+    });
   }
 
   /**
