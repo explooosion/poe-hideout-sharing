41ffa414c3ab93b51c77471808d9e43a28454c91
explooosion <ta7382@gmail.com>
2020-05-12 14:15:12 +0800

    Fix config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,8 +6,8 @@ import 'firebase/storage';
 
 // Mode
 console.info(`[${process.env.NODE_ENV.toUpperCase()} MODE]`);
-// export const REF_PICK = process.env.NODE_ENV === 'development' ? '_dev' : '';
-export const REF_PICK = '';
+export const REF_PICK = process.env.NODE_ENV === 'development' ? '_dev' : '';
+
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
